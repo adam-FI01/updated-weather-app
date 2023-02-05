3f8af82b472709e044a92eea90bfe64b23848b71
Adam Fine <adamfine@outlook.com>
2023-02-05 15:51:43 -0500

    probably final commit for now

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const axios = require('axios');
+const API_KEY = 'YOUR_API_KEY';
+const city = 'New York';
+const units = 'imperial'; // or 'metric' for Celsius
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${77705065a7de4afb8d5203705230502}`;
+
+axios.get(url)
+  .then(response => {
+    const weatherData = response.data;
+    console.log(`The temperature in ${city} is ${weatherData.main.temp}°F`);
+  })
+  .catch(error => {
+    console.error(error);
+  });
